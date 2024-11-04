bfbbf9facbbed40f826de71940ccf30960e2d07b
Lucas Henrique <lucashenriquesd@gmail.com>
2024-11-03 23:30:23 -0300

    chore: add swagger and basic global config module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Nestjs')
+    .setDescription('Nestjs project to with batteries included')
+    .setVersion('1.0')
+    .addTag('test')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, documentFactory);
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT') || 3000;
+
+  await app.listen(port);
 }
 bootstrap();
