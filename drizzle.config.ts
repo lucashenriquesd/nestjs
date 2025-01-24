db70e81e32cfdcd12af87533a921923ac933cd00
Lucas Henrique <lucashenriquesd@gmail.com>
2025-01-24 13:36:13 -0300

    fix: drizzle config now uses ConfigService instead of process.env

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,14 @@
-import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
+import { ConfigService } from '@nestjs/config';
+
+const configService = new ConfigService();
 
 export default defineConfig({
   out: './src/modules/drizzle/migrations',
   schema: './src/modules/drizzle/schema.ts',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: configService.get<string>('DATABASE_URL')!,
   },
   migrations: {
     schema: 'public',
